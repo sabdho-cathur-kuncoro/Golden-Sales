831663d6608eb766f2e8ea4d130898b6189bdbf4
Sabdho Cathur Kuncoro <rds-mobile@RDS-MOBILEs-MacBook-Pro.local>
2026-07-15 14:10:58 +0700

    use bearer PRD

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -21,7 +21,7 @@ export const Config = {
   URL: API_URL,
   BASE_URL: API_URL + "/sales",
   API_TIMEOUT: 60 * 1000,
-  BEARER: bearerUat,
+  BEARER: bearerPrd,
 };
 
 export const APIBASIC = create({
